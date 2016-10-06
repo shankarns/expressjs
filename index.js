0b747012ca1c2cf1602a54ed65f41226e3b555c0
Shankar <shankarn@gmail.com>
2016-10-06 16:52:13 +0800

    change 5

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var host = "localhost";
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, host);
-console.log('Server running at http://host:8080/');
+}).listen(3000, host);
+console.log('Server running at http://localhost:3000/');
