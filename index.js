6d0d9d90c859cef12f3b6b37e3a63de2e6a81255
Shankar <shankarn@gmail.com>
2016-10-06 15:50:55 +0800

    update2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from Cisco Shipped! & Shankar');
+    res.send('First express JS program');
 });
 
 var server = app.listen(3000, function () {
