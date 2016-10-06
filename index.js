a7f8de73dcb615dd6468a5b997fc2471b7695780
Shankar <shankarn@gmail.com>
2016-10-06 17:18:59 +0800

    commit 9

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var http = require('http');
 var host = "localhost";
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World 1\n');
 }).listen(3000, host);
-console.log('Server running at http://localhost:3000/');
