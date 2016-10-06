161c0f4e5ae9130d397aa635857f19c971786982
Shankar <shankarn@gmail.com>
2016-10-06 18:14:05 +0800

    change 10

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var host = "localhost";
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World 1\n');
+  res.end('Hello World 2\n');
 }).listen(3000, host);
